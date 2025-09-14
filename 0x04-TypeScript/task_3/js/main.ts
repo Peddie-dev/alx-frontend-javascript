330a7fcd36ac55dfd78efc941e94a5097e257877
Henry Omino <henryedwin92@gmail.com>
2025-09-14 23:34:19 +0300

    code update

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,28 @@
+/// <reference path="./crud.d.ts" />
+
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud";
+
+// Create a row object
+const row: RowElement = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+};
+
+// Insert the row and capture the generated ID
+const newRowID: RowID = CRUD.insertRow(row);
+
+// Update the row with an age field
+const updatedRow: RowElement = { ...row, age: 23 };
+
+// Call updateRow
+CRUD.updateRow(newRowID, updatedRow);
+
+// Call deleteRow
+CRUD.deleteRow(newRowID);
+
+
+
+
+
+
